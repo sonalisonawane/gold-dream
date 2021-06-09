05cea05977841853ee82832314c1f8baa39e3240
sonali sonawane <sonawanesonali41@gmail.com>
2021-06-09 18:46:01 +0530

    images added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // preloader
 const preloader = document.querySelector(".preloader");
 
-window.addEventListener("load", function () {
-  setTimeout(function () {
-    preloader.classList.add("hide-preloader");
-  }, 2000);
+window.addEventListener("load", function() {
+    setTimeout(function() {
+        preloader.classList.add("hide-preloader");
+    }, 2000);
 });
 
 // slides fade animation
@@ -12,42 +12,57 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides((slideIndex += n));
+    showSlides((slideIndex += n));
 }
-setInterval(function () {
-  plusSlides(1);
+setInterval(function() {
+    plusSlides(1);
 }, 3 * 1000);
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("img");
+    var i;
+    var slides = document.getElementsByClassName("img");
 
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "flex";
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "flex";
 }
 
 // slides fade animation end
 
 // background - color
 function scrollWin() {
-  bodyEl = document.querySelector("body");
-  linkEl = document.querySelectorAll(".nav-link");
-  if (window.pageYOffset >= 500) {
-    bodyEl.classList.add("changeColor");
-    bodyEl.style.color = "white";
-    linkEl.style.color = "white";
-  }
-  if (window.pageYOffset <= 500) {
-    bodyEl.classList.remove("changeColor");
-    bodyEl.style.color = "black";
-    linkEl.style.color = "black";
-  }
+    bodyEl = document.querySelector("body");
+    linkEl = document.querySelectorAll(".nav-link");
+    if (window.pageYOffset >= 500) {
+        bodyEl.classList.add("changeColor");
+        bodyEl.style.color = "white";
+        linkEl.style.color = "white";
+    }
+    if (window.pageYOffset <= 500) {
+        bodyEl.classList.remove("changeColor");
+        bodyEl.style.color = "black";
+        linkEl.style.color = "black";
+    }
 }
+
+let dayEl = document.querySelector("#day")
+DateEl = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+let day = weekday[DateEl.getDay()]
+
+dayEl.innerHTML = day + "..!";
